e5248a2142b91c3e845323ca80d5cca4a6dcf3d4
jjonatasx <111437851+jjonatasx@users.noreply.github.com>
2022-09-08 09:36:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcMult() {
+    let num1 = document.getElementsByName("n1")[0].value;
+
+    let num2 = document.getElementsByName("n1")[0].value;
+    
+    let res = Number (num1) * Number (num2);
+    document.getElementsByName("resultado")[0].value = res;
+}
